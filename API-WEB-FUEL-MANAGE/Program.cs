e085faf1b2fb229dd1466ba81d00b7f896f2c04f
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-03-22 19:16:28 -0300

    Adicionar arquivos de projeto.

---
+++ API-WEB-FUEL-MANAGE/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();//AQUI, JUNTAMENTE COM O CÓDIGO DENTRO DO BLOCO IF ESTAVA DANDO ERRO POIS NÃO INSTALEI A DEPENDÊNCIA DO SWAGGERGEN
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
