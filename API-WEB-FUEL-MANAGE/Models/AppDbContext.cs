0e71a3b21e47a75cb419abceeabb3ebda7179c0f
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-04-02 15:40:19 -0300

    Subindo AppDbContext e configuração do Db

---
+++ API-WEB-FUEL-MANAGE/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_WEB_FUEL_MANAGE.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+             
+        }
+     public DbSet<Veiculo> Veiculos { get; set; }
+     public DbSet<Consumo> Consumos { get; set; }
+
+    }
+
+}
--- API-WEB-FUEL-MANAGE/Program.cs
+++ API-WEB-FUEL-MANAGE/Program.cs
@@ -1,8 +1,15 @@
+using API_WEB_FUEL_MANAGE.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();//AQUI, JUNTAMENTE COM O CÓDIGO DENTRO DO BLOCO IF ESTAVA DANDO ERRO POIS NÃO INSTALEI A DEPENDÊNCIA DO SWAGGERGEN
