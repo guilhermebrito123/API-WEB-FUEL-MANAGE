0c5a1bf0f2484c036b7066603016f57247fced44
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-04-03 11:14:00 -0300

    Métodos de requisição http da classe veiculos

---
+++ API-WEB-FUEL-MANAGE/Controllers/VeiculosController.cs
@@ -0,0 +1,52 @@
+﻿using API_WEB_FUEL_MANAGE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_WEB_FUEL_MANAGE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Veiculos.ToListAsync();
+
+            return Ok(model);//Esse método retorna o status 200
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if(model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
+            {
+                return BadRequest(new {message = "Ano de fabricação e ano de modelo são obrigatórios devem ser maiores que 0"});
+            }
+
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new {id = model.Id}, model);//O método createdataction serve para retornar o status 201 "Created". no cabeçalho da resposta, aparece o caminho para acessar as informações do veiculo cadastrado
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if(model == null) NotFound();
+
+            return Ok(model);
+        }
+    }
+}
--- API-WEB-FUEL-MANAGE/Models/Veiculo.cs
+++ API-WEB-FUEL-MANAGE/Models/Veiculo.cs
@@ -16,9 +16,9 @@ namespace API_WEB_FUEL_MANAGE.Models
         [Required]
         public string Placa { get; set; }
         [Required]
-        public int AnoFabricacao { get; set; }
+        public int AnoFabricacao { get; set; }//Por mais que o campo esteja como required, por ser do tipo int, caso não insira nada, ele registra como 0, por isso devo colocar if atributo == 0, badRequest()
         [Required]
-        public int AnoModelo { get; set; }
+        public int AnoModelo { get; set; }//Por mais que o campo esteja como required, por ser do tipo int, caso não insira nada, ele registra como 0, por isso devo colocar if atributo == 0, badRequest()
 
         public ICollection<Consumo> Consumos { get; set; }
 
